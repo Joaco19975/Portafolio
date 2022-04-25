960d61b6a2e517868fe0b1688e1038c7914e7128
Joaco19975 <92689740+Joaco19975@users.noreply.github.com>
2022-04-25 09:12:25 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,37 +1,6 @@
 <?php
 
-class conexion {
- private $servidor = "localhost";
- private $usuario = "root";
- private $contrasenia = "";
- private $conexion ;
-
- public function __construct(){
-
-   try{
-      $this->conexion = new PDO("mysql:host=$this->servidor;dbname=album",$this->usuario,$this->contrasenia);
-      $this->conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-   }catch(PDOException $e){
-        return "falla en la conexion".$e ;
-
-    } 
-
- }
-
-     public function ejecutar($sql){ //Insertar/Delete/Actualizar
-       $this->conexion->exec($sql);
-   
-       return $this->conexion->lastInsertId();
-
-     }
-
-     public function consultar($sql){
-      $sentencia = $this->conexion->prepare($sql); //Toma la instruccion sql y la almacena en $sentencia
-      $sentencia->execute();
-       return $sentencia->fetchAll() ; //Retorna todos los registros
-     }
-
-}
+$conexion = mysqli_connect('localhost', 'root', '', 'album') OR exit("No se pudo establecer una conexion con la base de datos. Consulte con el administrador");
 
 
 
